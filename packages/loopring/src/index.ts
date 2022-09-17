750a3d9a475cbd85cf123e40d80bf63b73186f6e
Niklas Natter <niklas@sulu.io>
2022-09-17 13:06:18 +0200

    Add ether and add some dummy function

--- packages/loopring/src/index.ts
+++ packages/loopring/src/index.ts
@@ -1,3 +1,17 @@
-export function hello(who: string = 'world'): string {
-    return `Hello ${who}! `;
+import { ethers } from "ethers";
+
+export const getAssets = async (provider: ethers.providers.JsonRpcProvider) => {
+    return await provider.getBlock('latest');
 }
+
+const main = async () => {
+    const provider = new ethers.providers.JsonRpcProvider('https://rpc.ankr.com/eth');
+    console.log(await getAssets(provider));
+}
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
