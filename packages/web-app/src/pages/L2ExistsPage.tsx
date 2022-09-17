54e399801ca16f9b812872060bf06fefd3aad473
Piotr Roslaniec <p.roslaniec@gmail.com>
2022-09-17 18:47:25 +0200

    enable zksync on click

--- packages/web-app/src/pages/L2ExistsPage.tsx
+++ packages/web-app/src/pages/L2ExistsPage.tsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
+
 import { Container, Section, SectionRow } from "../components/base/base";
+import { Button } from "../components/base/Button";
 import { Title } from "../typography/Title";
 import { ZkSync } from "./ZkSync";
 
 export const L2ExitsPage = () => {
+  const [enableZkSync, setEnableZkSync] = useState(false);
+
   return (
     <Container>
       <Section>
@@ -10,7 +15,11 @@ export const L2ExitsPage = () => {
           <Title>SELECT AN L2 TO EXIT</Title>
         </SectionRow>
         <SectionRow>
-          <ZkSync />
+          {enableZkSync ? (
+            <ZkSync />
+          ) : (
+            <Button onClick={() => setEnableZkSync(true)}>ZkSync</Button>
+          )}
         </SectionRow>
       </Section>
     </Container>
