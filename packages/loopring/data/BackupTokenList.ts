13bd0038e482d75c9f1f1d96e145123e3a710886
Niklas Natter <niklas@sulu.io>
2022-09-17 18:14:37 +0200

    Impelement method for submitting withdrawal request

---
+++ packages/loopring/data/BackupTokenList.ts
@@ -0,0 +1,73 @@
+export const BackupTokenList = [
+  {
+    name: 'Ethereum',
+    symbol: 'ETH',
+    decimals: 18,
+    address: '0x0000000000000000000000000000000000000000',
+    tokenId: 0
+  },
+  {
+    name: 'Loopring',
+    symbol: 'LRC',
+    decimals: 18,
+    address: '0xfc28028d9b1f6966fe74710653232972f50673be',
+    tokenId: 1
+  },
+  {
+    name: 'USDT',
+    symbol: 'USDT',
+    decimals: 6,
+    address: '0xd4e71c4bb48850f5971ce40aa428b09f242d3e8a',
+    tokenId: 2
+  },
+  {
+    name: 'AMM-LRC-ETH',
+    symbol: 'LP-LRC-ETH',
+    decimals: 8,
+    address: '0xfeb069407df0e1e4b365c10992f1bc16c078e34b',
+    tokenId: 4
+  },
+  {
+    name: 'LP-ETH-USDT',
+    symbol: 'LP-ETH-USDT',
+    decimals: 8,
+    address: '0x049a02fa9bc6bd54a2937e67d174cc69a9194f8e',
+    tokenId: 7
+  },
+  {
+    name: 'dai',
+    symbol: 'DAI',
+    decimals: 18,
+    address: '0xcd2c81b322a5b530b5fa3432e57da6803b0317f7',
+    tokenId: 6
+  },
+  {
+    name: 'OLDUSDC',
+    symbol: 'OLDUSDC',
+    decimals: 6,
+    address: '0x47525e6a5def04c9a56706e93f54cc70c2e8f165',
+    tokenId: 8
+  },
+  {
+    name: 'LP-OLDUSDC-ETH',
+    symbol: 'LP-OLDUSDC-ETH',
+    decimals: 8,
+    address: '0xf37cf4ced77b985708d591acc6bfd08586ab3409',
+    tokenId: 9
+  },
+  {
+    name: 'Uniswap',
+    symbol: 'UNI',
+    decimals: 18,
+    address: '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984',
+    tokenId: 11
+  },
+  {
+    name: 'wst ETH',
+    symbol: 'WSTETH',
+    decimals: 18,
+    address: '0x4942bbaf745f235e525baff49d31450810eded5b',
+    tokenId: 12
+  }
+]
+;
--- packages/loopring/src/index.ts
+++ packages/loopring/src/index.ts
@@ -1,21 +1,75 @@
 import { ethers } from "ethers";
 import {ExchangeV3Abi} from '../abi/ExchangeV3.js';
+import * as LoopringSDK from "@loopring-web/loopring-sdk";
+import {BackupTokenList} from "../data/BackupTokenList.js";
 
-export const getWithdrawableEther = async (
-    provider: ethers.providers.JsonRpcProvider,
-    contractAddress: string,
-    ownerAddress: string
+const goerliExchangeContractAddress = '0x2e76EBd1c7c0C8e7c2B875b6d505a260C525d25e';
+
+export const getAccountInformation = async (accountAddress: string) => {
+    const api = new LoopringSDK.ExchangeAPI({chainId: LoopringSDK.ChainId.GOERLI});
+    const response = await api.getAccount({owner: accountAddress});
+
+    return response.accInfo;
+}
+
+export const getAccountId = async (accountAddress: string) => {
+    const accountInformation = await getAccountInformation(accountAddress);
+
+    return accountInformation?.accountId;
+}
+
+export const getAllTokens = async () => {
+    const api = new LoopringSDK.ExchangeAPI({chainId: LoopringSDK.ChainId.GOERLI});
+    const response = await api.getTokens();
+
+    const tokens = [];
+    if (response.tokensMap) {
+        for (const apiToken of Object.values(response.tokensMap)) {
+            tokens.push({
+                name: apiToken.name,
+                symbol: apiToken.symbol,
+                decimals: apiToken.decimals,
+                address: apiToken.address,
+                tokenId: apiToken.tokenId,
+            });
+        }
+    } else {
+        tokens.push(...BackupTokenList)
+    }
+
+    return tokens;
+}
+
+export const submitWithdrawalTransaction = async (
+    wallet: ethers.Wallet,
+    tokenAddress: string,
 ) => {
-    const exchangeContract = new ethers.Contract(contractAddress, ExchangeV3Abi, provider);
+    const exchangeContract = new ethers.Contract(goerliExchangeContractAddress, ExchangeV3Abi, wallet);
 
-    return exchangeContract.getAmountWithdrawable(ownerAddress, ethers.constants.AddressZero)
+    const withdrawalFee = ethers.utils.parseUnits('0.02', 18);
+    const accountId = await getAccountId(wallet.address);
+    const transaction = exchangeContract.forceWithdraw(
+        wallet.address,
+        tokenAddress,
+        accountId,
+        {value: withdrawalFee}
+    ) as ethers.ContractTransaction;
+
+    return transaction;
 }
 
 const main = async () => {
-    const goerliContractAddress = '0x2e76EBd1c7c0C8e7c2B875b6d505a260C525d25e'
-    const provider = new ethers.providers.JsonRpcProvider('https://rpc.ankr.com/eth_goerli/');
-    const withdrawableEther = await getWithdrawableEther(provider, goerliContractAddress, '0xe6e19bC2811842fBb264E13a7DB4bFAD4902946f');
-    console.log(ethers.utils.formatUnits(withdrawableEther, 18));
+    const provider = new ethers.providers.JsonRpcProvider('https://rpc.ankr.com/eth_goerli');
+    const wallet = new ethers.Wallet(
+        '45c6c6ed80e3bcf20df86f53edc34bb408d66adf6464e8d31712203825e7ac74',
+        provider
+    );
+
+    const tx = await submitWithdrawalTransaction(wallet, ethers.constants.AddressZero);
+    console.log(tx);
+
+    console.log('waiting for inclusion');
+    await tx.wait();
 }
 main()
     .then(() => process.exit(0))
